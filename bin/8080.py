538b694da22dbf064daaf8fd1627f988498cf8bc
Zvonimir Rudinski <zvonimirurdinski@protonmail.ch>
2017-03-28 17:38:36 +0200

    Update 8080.py

--- bin/8080.py
+++ bin/8080.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 '''
-Intel 8080 Assembler napisan u Python 2.7
-Svi sistemi su podrzani.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-Autor: Zvonimir Rudinski
-Datum: 18.02.2017
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 from os import path
 from sys import argv,exit
