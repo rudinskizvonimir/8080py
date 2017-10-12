ab4a219c69d9363def08ec189c01f0a5b4743add
Ryan Jarvis <ryan@shopboxretail.com>
2017-10-11 23:11:54 -0700

    add a simple test to make sure we are still creating valid output

---
+++ tests/basic_tests.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+import unittest
+from binascii import hexlify
+import os
+
+from subprocess import Popen, PIPE
+
+
+class SimpleRegressionTests(unittest.TestCase):
+    """Some simple tests to make sure nothing major breaks while refactoring."""
+
+    def test_no_data_corruption(self):
+        with open("data/original_memcpy.rom", 'rb') as original_output:
+            original_hex = hexlify(original_output.read())
+
+        x = Popen(['python', '../bin/8080.py', '../examples/memcpy'], stdout=PIPE)
+        print(x.stdout.read())
+        with open('../examples/memcpy.rom', 'rb') as new_output:
+            new_hex = hexlify(new_output.read())
+
+        assert original_hex == new_hex
+
+        os.remove('../examples/memcpy.rom')
